13216e8b3c6352614ea3dd8ea38e8f75e844e58b
Smytt <parushev.kiril@gmail.com>
2018-08-16 16:48:43 +0300

    Models Extension, User, Tag; Appconfig

---
+++ src/main/java/com/tick42/quicksilver/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.tick42.quicksilver.config;
+
+import com.tick42.quicksilver.models.Extension;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public SessionFactory createSessionFactory() {
+        return new org.hibernate.cfg.Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Extension.class)
+                .buildSessionFactory();
+    }
+}
---
+++ src/main/java/com/tick42/quicksilver/models/Tag.java
@@ -0,0 +1,53 @@
+package com.tick42.quicksilver.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "extension_tags",
+            joinColumns = @JoinColumn(name = "tag_id"),
+            inverseJoinColumns = @JoinColumn(name = "extension_id"))
+    private List<Extension> extensions = new ArrayList<>();
+
+    public Tag() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Extension> getExtensions() {
+        return extensions;
+    }
+
+    public void setExtensions(List<Extension> extensions) {
+        this.extensions = extensions;
+    }
+
+
+}
