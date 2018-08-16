e338e1710e54a187888c90bf3df2b747e7b31ae7
Smytt <parushev.kiril@gmail.com>
2018-08-16 13:04:06 +0300

    initial commit + gradle dependencies

---
+++ src/main/java/com/tick42/quicksilver/QuicksilverApplication.java
@@ -0,0 +1,12 @@
+package com.tick42.quicksilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicksilverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicksilverApplication.class, args);
+	}
+}
