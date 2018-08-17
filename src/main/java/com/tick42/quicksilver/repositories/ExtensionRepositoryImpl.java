5f5659716e3aa97ba188a0e19d796925854180bb
Smytt <parushev.kiril@gmail.com>
2018-08-17 13:16:52 +0300

    ext repo, user repo

--- src/main/java/com/tick42/quicksilver/repositories/ExtensionRepositoryImpl.java
+++ src/main/java/com/tick42/quicksilver/repositories/ExtensionRepositoryImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ExtensionRepositoryImpl implements GenericRepository<Extension> {
     @Override
     public void create(Extension model) {
-        
+
     }
 
     @Override
