da862006d507d23e2fa10458fc05f212d998aa0d
Smytt <parushev.kiril@gmail.com>
2018-08-17 13:09:13 +0300

    refCTOR

--- src/main/java/com/tick42/quicksilver/config/AppConfiguration.java
+++ src/main/java/com/tick42/quicksilver/config/AppConfiguration.java
@@ -1,6 +1,10 @@
 package com.tick42.quicksilver.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.tick42.quicksilver.models.Extension;
+import com.tick42.quicksilver.models.Tag;
+import com.tick42.quicksilver.models.User;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +21,16 @@ import java.beans.PropertyVetoException;
 public class AppConfiguration {
     @Autowired
     private Environment env;
+
+    @Bean
+    public SessionFactory createSessionFactory() {
+        return new org.hibernate.cfg.Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Extension.class)
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Tag.class)
+                .buildSessionFactory();
+    }
 
     @Bean
     public DataSource securityDataSource() {
@@ -39,11 +53,11 @@ public class AppConfiguration {
 
         return securityDataSource;
     }
+    
     @Bean
     public UserDetailsManager userDetailsManager() {
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
         jdbcUserDetailsManager.setDataSource(securityDataSource());
         return jdbcUserDetailsManager;
     }
-
 }
