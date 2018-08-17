b1ffba5fd41cf164fc4553c53c9a0df0f09b537a
Smytt <parushev.kiril@gmail.com>
2018-08-17 13:15:49 +0300

    ext repo, user repo

---
+++ src/main/java/com/tick42/quicksilver/repositories/ExtensionRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.tick42.quicksilver.repositories;
+
+import com.tick42.quicksilver.models.Extension;
+import com.tick42.quicksilver.repositories.base.GenericRepository;
+
+import java.util.List;
+
+public class ExtensionRepositoryImpl implements GenericRepository<Extension> {
+    @Override
+    public void create(Extension model) {
+        
+    }
+
+    @Override
+    public void update(int id, Extension model) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Extension> findAll() {
+        return null;
+    }
+
+    @Override
+    public Extension findById(int id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/tick42/quicksilver/repositories/UserRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.tick42.quicksilver.repositories;
+
+import com.tick42.quicksilver.models.User;
+import com.tick42.quicksilver.repositories.base.GenericRepository;
+
+import java.util.List;
+
+public class UserRepositoryImpl implements GenericRepository<User> {
+    @Override
+    public void create(User model) {
+
+    }
+
+    @Override
+    public void update(int id, User model) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(int id) {
+        return null;
+    }
+}
