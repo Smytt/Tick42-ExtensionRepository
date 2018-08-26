421a59754444fd6a960c0d857bab6cc55e49dc88
Smytt <parushev.kiril@gmail.com>
2018-08-26 10:09:17 +0300

    scheduling off

--- src/main/java/com/tick42/quicksilver/QuicksilverApplication.java
+++ src/main/java/com/tick42/quicksilver/QuicksilverApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class QuicksilverApplication {
 
 	public static void main(String[] args) {
