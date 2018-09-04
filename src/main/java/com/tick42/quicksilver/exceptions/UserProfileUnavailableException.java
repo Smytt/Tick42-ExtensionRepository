e2918ecae878024b253eba7827dd9f6d04d06b23
Smytt <parushev.kiril@gmail.com>
2018-09-04 17:19:23 +0300

    user profile unavailable if user disabled

---
+++ src/main/java/com/tick42/quicksilver/exceptions/UserProfileUnavailableException.java
@@ -0,0 +1,8 @@
+package com.tick42.quicksilver.exceptions;
+
+public class ExtensionUnavailableException extends RuntimeException {
+
+    public ExtensionUnavailableException(String exception) {
+        super(exception);
+    }
+}
