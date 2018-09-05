30db09526054b21d6f96a9f5ce5458ba3ff2f0b5
Smytt <parushev.kiril@gmail.com>
2018-09-05 23:03:53 +0300

    unit test extension set featured, extension find pending

--- src/test/java/com/tick42/quicksilver/services/ExtensionServiceImplTests.java
+++ src/test/java/com/tick42/quicksilver/services/ExtensionServiceImplTests.java
@@ -1,5 +1,6 @@
 package com.tick42.quicksilver.services;
 
+import com.tick42.quicksilver.exceptions.InvalidStateException;
 import com.tick42.quicksilver.models.DTO.ExtensionDTO;
 import com.tick42.quicksilver.models.Extension;
 import com.tick42.quicksilver.repositories.base.ExtensionRepository;
@@ -12,50 +13,87 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExtensionServiceImplTests {
     @Mock
     private ExtensionRepository extensionRepository;
-    @Mock
-    TagService tagService;
-    @Mock
-    GitHubService gitHubService;
-    @Mock
-    JwtValidator jwtValidator;
-    @Mock
-    UserRepository userRepository;
 
-    private ExtensionService extensionService;
-
-    @Before
-    public void generateExtensionService() {
-        extensionService = new ExtensionServiceImpl(extensionRepository, tagService, gitHubService, jwtValidator, userRepository);
-    }
-
+    @InjectMocks
+    private ExtensionServiceImpl extensionService;
 
     @Test
     public void setFeaturedState_whenSetToFeatured_returnFeaturedExtensionDTO() {
         // Arrange
-        Extension extensionBeforeUpdate = new Extension();
-        extensionBeforeUpdate.setIsFeatured(false);
+        Extension extension = new Extension();
+        extension.setIsFeatured(false);
 
-        when(extensionRepository.findById(1)).thenReturn(extensionBeforeUpdate);
+        when(extensionRepository.findById(1)).thenReturn(extension);
 
         //Act
-        ExtensionDTO extensionAfterUpdate = extensionService.setFeaturedState(1, "feature");
+        ExtensionDTO extensionShouldBeFeatured = extensionService.setFeaturedState(1, "feature");
 
         //Assert
-        Assert.assertTrue(extensionAfterUpdate.isFeatured());
+        Assert.assertTrue(extensionShouldBeFeatured.isFeatured());
     }
 
     @Test
-    public void test() {
-        Assert.assertTrue(true);
+    public void setFeaturedState_whenSetToUnfeatured_returnUnfeaturedExtensionDTO() {
+        // Arrange
+        Extension extension = new Extension();
+        extension.setIsFeatured(true);
+
+        when(extensionRepository.findById(1)).thenReturn(extension);
+
+        //Act
+        ExtensionDTO extensionShouldBeUnfeatured = extensionService.setFeaturedState(1, "unfeature");
+
+        //Assert
+        Assert.assertFalse(extensionShouldBeUnfeatured.isFeatured());
+    }
+
+    @Test(expected = InvalidStateException.class)
+    public void setFeaturedState_whenGivenInvalidParameter_shouldThrow() {
+        // Arrange
+        Extension extension = new Extension();
+        extension.setIsFeatured(true);
+
+        when(extensionRepository.findById(1)).thenReturn(extension);
+
+        //Act
+        ExtensionDTO extensionShouldThrow = extensionService.setFeaturedState(1, "wrong-string");
+
+        //Assert
+    }
+
+    @Test
+    public void findPending_shouldReturnListOfPendingExtensionDTOs() {
+        //Arrange
+        Extension extensionPending1 = new Extension();
+        Extension extensionPending2 = new Extension();
+        extensionPending1.setIsPending(true);
+        extensionPending2.setIsPending(true);
+        List<Extension> extensions = Arrays.asList(extensionPending1, extensionPending2);
+
+        when(extensionRepository.findPending()).thenReturn(extensions);
+
+        //Act
+        List<ExtensionDTO> pendingExtensionDTOs = extensionService.findPending();
+
+        //Assert
+        Assert.assertEquals(2, pendingExtensionDTOs.size());
+        Assert.assertTrue(pendingExtensionDTOs.get(0).isPending());
+        Assert.assertTrue(pendingExtensionDTOs.get(1).isPending());
     }
 }
