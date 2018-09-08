2dbc2a4cf0e92e1bb9b1e0ffa1f8f1772c6c0848
Smytt <parushev.kiril@gmail.com>
2018-09-08 14:34:39 +0300

    unit test git hub set params

--- src/test/java/com/tick42/quicksilver/services/GitHubServiceImplTests.java
+++ src/test/java/com/tick42/quicksilver/services/GitHubServiceImplTests.java
@@ -1,10 +1,13 @@
 package com.tick42.quicksilver.services;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.tick42.quicksilver.exceptions.GitHubRepositoryException;
+import com.tick42.quicksilver.exceptions.InvalidParameterException;
 import com.tick42.quicksilver.models.GitHubModel;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kohsuke.github.GHFileNotFoundException;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -15,7 +18,7 @@ public class GitHubServiceImplTests {
     private GitHubServiceImpl gitHubService;
 
     @Test
-    public void setRemoteDetails_GitHubModelIsInvalid_shouldThrow() {
+    public void setRemoteDetails_whenGitHubModelIsValid_shouldNotThrow() {
         //Arrange
         GitHubModel gitHubModel = new GitHubModel();
         gitHubModel.setUser("Smytt");
@@ -30,5 +33,37 @@ public class GitHubServiceImplTests {
             Assert.fail(e.getMessage());
         }
 
+    }
+
+    @Test
+    public void setRemoteDetails_whenGitHubModelIsInvalid_shouldSetFailMsgAndDate() {
+        //Arrange
+        GitHubModel gitHubModel = new GitHubModel();
+        gitHubModel.setUser("some");
+        gitHubModel.setRepo("other");
+
+        //Act
+        gitHubService.setRemoteDetails(gitHubModel);
+
+        //Asser
+        Assert.assertNotNull(gitHubModel.getFailMessage());
+        Assert.assertNotNull(gitHubModel.getLastFail());
+    }
+
+    @Test
+    public void setRemoteDetails_whenGitHubModelIsValid_shouldSetDetails() {
+        //Arrange
+        GitHubModel gitHubModel = new GitHubModel();
+        gitHubModel.setUser("Smytt");
+        gitHubModel.setRepo("Tick42-ExtensionRepository");
+
+        //Act
+        gitHubService.setRemoteDetails(gitHubModel);
+
+        //Asser
+        Assert.assertNotNull(gitHubModel.getLastSuccess());
+        Assert.assertNotNull(gitHubModel.getLastCommit());
+        Assert.assertNotNull(gitHubModel.getPullRequests());
+        Assert.assertNotNull(gitHubModel.getOpenIssues());
     }
 }
